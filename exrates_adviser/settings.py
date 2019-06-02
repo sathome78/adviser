b132d3e8dc5b98d94959d874d741e72993c0a44c
Probachai Yulia <probachai.yulia@upholding.biz>
2019-06-02 17:10:02 +0300

    added settings

--- exrates_adviser/settings.py
+++ exrates_adviser/settings.py
@@ -170,6 +170,10 @@ LOCALE_PATHS = (
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.getenv('STATIC_ROOT_DIRECROTY', os.path.join(BASE_DIR, 'staticfiles'))
+#STATIC_ROOT = os.getenv('STATIC_ROOT_DIRECTORY', os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
