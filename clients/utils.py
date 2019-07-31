01520aaf00f07fb5b8c1ed7f97731f60dd53b2e1
Probachai Yulia <probachai.yulia@upholding.biz>
2019-07-31 21:59:09 +0300

    migrations

--- clients/utils.py
+++ clients/utils.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 CONFIG_PROFILE_PATH = os.environ.get('CONFIG_PROFILE_PATH', None)
 if CONFIG_PROFILE_PATH is None:
-    CONFIG_PATH = os.path.join(BASE_DIR, 'config', 'prod.ini')
+    CONFIG_PATH = os.path.join(BASE_DIR, 'config', 'local.ini')
     logging.info("using {} config file".format(CONFIG_PATH))
 else:
     logging.info("using {} config file".format(CONFIG_PROFILE_PATH))
